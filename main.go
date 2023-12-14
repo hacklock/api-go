d5d1cf84c8cfe3be5f6747d17f292757a8b438f9
Wilton R. Cruz <jiquitaiasoftware@gmail.com>
2023-12-13 21:01:28 -0300

    feat: add router subpackage

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
-package main 
+package main
 
-import(
-	"github.com/gin-gonic/gin"
+import (
+	
+	"github.com/hacklock/api-go.git/router"
 )
 
 func main() {
-	// Inicializa o Router na config default do gin
-	r  := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// Initialize Router
+	router.Inicialize()
 }
