291161a152d42beabb631a416635441c4af3ee92
Wilton R. Cruz <jiquitaiasoftware@gmail.com>
2023-12-13 21:03:14 -0300

    feat: add router subpackage

--- router/router.go
+++ router/router.go
@@ -3,7 +3,7 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Inicialize() {
-	// Inicializa o Router na config default do gin
+	// Inicializa o Router na config default do gin.
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
